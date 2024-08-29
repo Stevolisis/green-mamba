3bef95e644ce1dffb683475bebfcd7244c927061
Steven Joseph <66651054+Stevolisis@users.noreply.github.com>
2024-08-29 22:09:25 +0100

    close slider added to slide UI

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,23 +1,29 @@
 "use client"
-import { useAppSelector } from '@/redux/hooks'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import React from 'react'
 import CompleteProfile from './CompleteProfile';
+import { FaTimesCircle } from 'react-icons/fa';
+import { showSlide } from '@/redux/slices/profile';
 
 
 const Slider = () => {
   const { slideUp, type } = useAppSelector((state)=> state.profile);
+  const dispatch = useAppDispatch();
 
   return (
     <>
       <div className={`${slideUp && "glassMorphism !flex"} hidden transition-all duration-300 z-[49] justify-center items-center w-full h-full fixed bottom-0`}></div>
           
       <div className={`transition-transform duration-300 ease-in-out 
-      ${
-        slideUp ? "transform translate-y-0" : "transform translate-y-full"
-      } 
-      overflow-auto scrollbar-none sm:scrollbar-thin !z-[49] flex flex-col justify-start items-center w-full fixed h-[75vh] bottom-0 rounded-tr-[28px] 
-      rounded-tl-[28px] bg-slideUp`}
+        ${
+          slideUp ? "transform translate-y-0" : "transform translate-y-full"
+        } 
+        overflow-auto scrollbar-none sm:scrollbar-thin !z-[52] flex flex-col justify-start items-center w-full fixed h-[75vh] sm:h-[96vh] bottom-0 rounded-tr-[28px] 
+        rounded-tl-[28px] bg-slideUp`}
       >
+        <button className='absolute right-6 top-6' onClick={()=> dispatch(showSlide())}>
+          <FaTimesCircle size={26} className='text-bgSecondary'/>
+        </button>
         {
           type === "complete_profile" ? 
             <CompleteProfile/>
