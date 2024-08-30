d6a9198b3b4c37c62b681025dd17182d28ba66f4
Steven Joseph <66651054+Stevolisis@users.noreply.github.com>
2024-08-30 23:56:40 +0100

    Dashboard Summary

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,44 +1,103 @@
 "use client"
+import Link from 'next/link'
 import React from 'react'
 import { BsBank2 } from 'react-icons/bs'
-import { IoStatsChartSharp } from 'react-icons/io5'
+import { IoNotifications, IoStatsChartSharp } from 'react-icons/io5'
+import { MdArticle } from 'react-icons/md'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div className="px-4 sm:px-16 py-12">
-      <div>
+    <div className="px-4 sm:px-16 py-12 font-[SatoshiRegular]">
+      <div className='flex justify-between gap-3 flex-wrap sm:flex-nowrap'>
 
-        <div>
-          <div>
+        <div className='w-full p-7 rounded-lg shadow-lg bg-gradient-to-br from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% flex flex-col gap-y-14'>
+          <div className='flex justify-between items-center'>
             
-            <div>
+            <div className='rounded-md p-3 glassMorphism'>
               <BsBank2 size={18} />
             </div>
 
             <div>
-              <p>Steven Joseph</p>
+              <p className="text-sm">Steven Joseph</p>
             </div>
           </div>
 
-          <div>
+          <div className='flex justify-between items-baseline'>
             
             <div>
-              <p>
+              <p className="text-sm mb-2">
                 Balance
               </p>
-              <h2>
+              <h2 className="text-3xl font-[SatoshiBold]">
                 12,455 ETH
               </h2>
             </div>
 
-            <div>
-              <IoStatsChartSharp size={24} />
-            </div>
+            {/* <div className='h-full'>
+              <IoStatsChartSharp className='text-[50px] opacity-50' />
+            </div> */}
           </div>
         </div>
 
+        <div className='w-full p-7 rounded-lg shadow-lg bg-gradient-to-r from-[#E27C39] to-[#E3FF73] to-90% glassMorphism border border-[#ffffff1a] flex flex-col gap-y-14'>
+          <div className='flex justify-between items-center'>
+            
+            <div className='rounded-md p-3 glassMorphism'>
+              <MdArticle size={18} />
+            </div>
+
+            <div>
+              <Link href="/articles" className="text-sm underline">View all</Link>
+            </div>
+          </div>
+
+          <div className='flex justify-between items-baseline'>
+            
+            <div>
+              <p className="text-sm mb-2">
+                Number Of Articles
+              </p>
+              <h2 className="text-3xl font-[SatoshiBold]">
+                43
+              </h2>
+            </div>
+
+            {/* <div className='h-full'>
+              <IoStatsChartSharp className='text-[50px] opacity-50' />
+            </div> */}
+          </div>
+        </div>
+
+        <div className='w-full p-7 rounded-lg shadow-lg bg-gradient-to-r from-[#71244e] to-[#ff4a64] to-90% glassMorphism border border-[#ffffff1a] flex flex-col gap-y-14'>
+          <div className='flex justify-between items-center'>
+            
+            <div className='rounded-md p-3 glassMorphism'>
+              <IoNotifications size={18} />
+            </div>
+
+            <div>
+              <Link href="/articles" className="text-sm underline">View all</Link>
+            </div>
+          </div>
+
+          <div className='flex justify-between items-baseline'>
+            
+            <div>
+              <p className="text-sm mb-2">
+                Recent Notification
+              </p>
+              <h2 className="text-3xl font-[SatoshiBold]">
+                17
+              </h2>
+            </div>
+
+            {/* <div className='h-full'>
+              <IoStatsChartSharp className='text-[50px] opacity-50' />
+            </div> */}
+          </div>
+        </div>
       </div>
     </div>
   )
