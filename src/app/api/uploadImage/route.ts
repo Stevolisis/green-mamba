0fdbba381424c2e4b18936248f9d82519cfef74a
Steven Joseph <66651054+Stevolisis@users.noreply.github.com>
2024-09-22 17:18:02 +0100

    pinata file upload unsuccessful, returning 403 usage limit

--- src/app/api/uploadImage/route.ts
+++ src/app/api/uploadImage/route.ts
@@ -1,11 +1,23 @@
+import { pinata } from "@/utils/pinataConfig";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {  
     try {  
-        const body = await req.formData()
-        console.log(body.get("image"))
-        return NextResponse.json({ data: "Is Working", message:body },{ status: 202});  
-    } catch (error) {  
+        const body = await req.formData();
+        const file:File | null = body.get("file") as unknown as File;
+        
+        if(file){
+            const uploadFile = pinata.upload.file(file);
+            console.log("333 ",uploadFile);
+            const url = await pinata.gateways.convert((await uploadFile).IpfsHash);
+            return NextResponse.json({ data: url, message:"Image Uploaded Successfully" },{ status: 200});  
+
+        }else{
+            return NextResponse.json({ message:"File not found" },{ status: 404});  
+        }
+    
+    } catch (error:any) {  
+        console.log("Error. ",error.message);
         return NextResponse.json({ error });  
     }  
 }
---
+++ src/utils/pinataConfig.ts
@@ -0,0 +1,7 @@
+"server only"
+import { PinataSDK } from "pinata-web3";
+
+export const pinata = new PinataSDK({
+    pinataJwt:process.env.PINATA_JWT,
+    pinataGateway:process.env.PINATA_GATEWAY
+});
