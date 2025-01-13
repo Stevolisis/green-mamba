19100c927cf1dc6187db5c0417df0f73b26635ce
Steven Joseph <66651054+Stevolisis@users.noreply.github.com>
2025-01-13 01:27:44 +0100

    api set to normal, cloudinary upgrade fix upload stream bug

--- src/app/api/articles/createArticle/route.ts
+++ src/app/api/articles/createArticle/route.ts
@@ -22,21 +22,20 @@ export async function POST(req: NextRequest) {
                 const slugged = slugify(slug.replace(/[^\w\s']|_/g,' ').replaceAll("'",' '));
                 const tags = JSON.parse(body.get("tags") as string);
                 console.log("ImgUpload: ", imgData);
-                console.log('File type:', file.type);
-                console.log('File size:', file.size);
-                // const newArticle= new Article({
-                //     title: body.get("title"),
-                //     slug: slugged,
-                //     description: body.get("description"),
-                //     img:{ public_id:'imgData.public_id', url:'imgData.secure_url' },
-                //     content: body.get("content"),
-                //     author: authorId,
-                //     tags: tags,
-                //     gifts: 0
-                // });
+
+                const newArticle= new Article({
+                    title: body.get("title"),
+                    slug: slugged,
+                    description: body.get("description"),
+                    img:{ public_id:imgData.public_id, url:imgData.secure_url },
+                    content: body.get("content"),
+                    author: authorId,
+                    tags: tags,
+                    gifts: 0
+                });
     
-                // const addArticle= await newArticle.save();
-                return NextResponse.json({ data: imgData, message:"Article posted Successfully" },{ status: 200});      
+                const addArticle= await newArticle.save();
+                return NextResponse.json({ data: addArticle, message:"Article posted Successfully" },{ status: 200});      
             }else{
                 return NextResponse.json({ message:"AuthorId not found" },{ status: 404});
             }
