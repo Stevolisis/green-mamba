d2814ae551e4dbadf7e9d485b4d5083d607455aa
Steven Joseph <66651054+Stevolisis@users.noreply.github.com>
2025-05-09 01:16:50 +0100

    default to light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <>
-    <html lang="en">
+    <html lang="en" className="light">
       <body className="bg-bgPrimary h-auto">
         <AppKitProvider>
           <ReactProvider>
